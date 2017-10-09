865561599fd6ad5b66ca62414cf5f2541d59e2a1
Emanuele Ianni <dierre@gmail.com>
2017-10-09 21:55:15 +0200

    rethinkdb

---
+++ README.md
@@ -0,0 +1,10 @@
+*Learning Javascript!*
+
+
+Stuff to be done:
+
+- use promises
+- install rethinkdb
+- implement `/start`
+- implement `/unsubscribe`
+- implement `/adminbardoculo`
--- main/app.js
+++ main/app.js
@@ -1,6 +1,21 @@
-var express = require('express');
+const express = require('express');
+const r = require('rethinkdb');
+const promisify = require("es6-promisify");
 
 var app = express();
+
+// const db_connect = promisify(r.connect);
+
+// db_connect({ host: '127.0.0.1', port: 28015})
+//     .then(function (connection) {
+//         console.log(connection);
+//         r.db('test').tableCreate('authors').run(connection, function(err, result) {
+//             console.log(JSON.stringify(result, null, 2));
+//         });
+//     }).catch(function (err) {
+//         console.error('Ops!', err);
+// });
+
 
 const TelegramBot = require('node-telegram-bot-api');
 
@@ -9,36 +24,37 @@ const token = '478152078:AAG2PcGOOc1NGaEm5n5BKuBU6N8OBI9yTQ8';
 const bot = new TelegramBot(token, {polling: true});
 
 app.get('/', function (req, res) {
-    res.send('Hello World')
+    res.send('Hello World');
 });
 
 app.listen(8080, function () {
-    console.log('App listening on port 8080!')
+    console.log('App listening on port 8080!');
 });
 
 module.exports = app;
 
-bot.onText(/\/adminebardoculo (.+)/, function onEchoText(msg, match) {
-    const resp = match[1];
-    console.log("sending to everybody " + resp);
-    bot.sendMessage(msg.chat.id, resp);
+bot.onText(/\/adminbardoculo (.+) (.+)/, function onEchoText(msg, match) {
+    const resp1 = match[1];
+    const resp2 = match[2];
+    console.log("sending to everybody " + resp1);
+    console.log("sending to everybody " + resp2);
 });
 
 bot.onText(/\/ping/, function onEchoText(msg) {
-    console.log("pong!");
+    console.log("pong from chat id: " + msg.chat.id );
     bot.sendMessage(msg.chat.id, "pong!");
 });
 
 bot.onText(/\/start/, function onEchoText(msg) {
-    console.log("saving..." + chatId);
+    console.log("saving... " + msg.chat.id);
     bot.sendMessage(msg.chat.id, "*Welcome!*");
 });
 
 bot.onText(/\/unsubscribe/, function onEchoText(msg) {
-    console.log("saving..." + chatId);
-    bot.sendMessage(msg.chat.id, "*Welcome!*");
+    console.log("unsubscribing... " + chatId);
+    bot.sendMessage(msg.chat.id, "*Sad to see you leave! Hope you come back soon :)*");
 });
 
 bot.on('message', function (msg) {
-    console.log("message " + chatId);
+    console.log(JSON.stringify(msg, null, 2));
 });
