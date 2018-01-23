da2036fd1b325b6a35d005dd26441cb19b316675
Emanuele Ianni <emanuele.ianni@lastminute.com>
2018-01-23 18:23:03 +0100

    removed unused dependencies

--- main/lokijsInit.js
+++
@@ -1,9 +0,0 @@
-function initCollection(db, TABLE) {
-    let table = db.getCollection(TABLE);
-    if (table === null) {
-        table = db.addCollection(TABLE);
-    }
-    return table;
-}
-
-module.exports = initCollection;
